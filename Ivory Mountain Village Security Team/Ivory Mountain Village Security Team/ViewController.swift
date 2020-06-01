8cb933172fc3faa0bc5c7d2f651726d5241bb838
Zhen Yang <webyangfullstack@gmail.com>
2020-06-01 17:59:49 +1000

    create a new project

---
+++ Ivory Mountain Village Security Team/Ivory Mountain Village Security Team/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ivory Mountain Village Security Team
+//
+//  Created by Zhen Yang on 1/6/20.
+//  Copyright © 2020 Zhen Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
